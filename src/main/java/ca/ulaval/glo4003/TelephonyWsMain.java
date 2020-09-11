b013b37ac4c6a69f0c7c7624e1016297b49e604e
Fabien Roy <fabienroy28@gmail.com>
2020-09-10 20:15:18 -0400

    [FIX] Setup call log resource for it to work

--- src/main/java/ca/ulaval/glo4003/TelephonyWsMain.java
+++ src/main/java/ca/ulaval/glo4003/TelephonyWsMain.java
@@ -1,12 +1,20 @@
 package ca.ulaval.glo4003;
 
+import ca.ulaval.glo4003.ws.api.calllog.CallLogResource;
+import ca.ulaval.glo4003.ws.api.calllog.CallLogResourceImpl;
 import ca.ulaval.glo4003.ws.api.contact.ContactResource;
 import ca.ulaval.glo4003.ws.api.contact.ContactResourceImpl;
+import ca.ulaval.glo4003.ws.domain.calllog.CallLog;
+import ca.ulaval.glo4003.ws.domain.calllog.CallLogAssembler;
+import ca.ulaval.glo4003.ws.domain.calllog.CallLogRepository;
+import ca.ulaval.glo4003.ws.domain.calllog.CallLogService;
 import ca.ulaval.glo4003.ws.domain.contact.Contact;
 import ca.ulaval.glo4003.ws.domain.contact.ContactAssembler;
 import ca.ulaval.glo4003.ws.domain.contact.ContactRepository;
 import ca.ulaval.glo4003.ws.domain.contact.ContactService;
 import ca.ulaval.glo4003.ws.http.CORSResponseFilter;
+import ca.ulaval.glo4003.ws.infrastructure.calllog.CallLogDevDataFactory;
+import ca.ulaval.glo4003.ws.infrastructure.calllog.CallLogRepositoryInMemory;
 import ca.ulaval.glo4003.ws.infrastructure.contact.ContactDevDataFactory;
 import ca.ulaval.glo4003.ws.infrastructure.contact.ContactRepositoryInMemory;
 import org.eclipse.jetty.server.Handler;
@@ -33,20 +41,17 @@ public class TelephonyWsMain {
   public static void main(String[] args)
           throws Exception {
 
-    // Setup resources (API)
     ContactResource contactResource = createContactResource();
-    // TODO something to do here!
-    createCallLogResource();
+    CallLogResource callLogResource = createCallLogResource();
 
-    // Setup API context (JERSEY + JETTY)
     ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
     context.setContextPath("/api/");
     ResourceConfig resourceConfig = ResourceConfig.forApplication(new Application() {
       @Override
       public Set<Object> getSingletons() {
         HashSet<Object> resources = new HashSet<>();
-        // Add resources to context
         resources.add(contactResource);
+        resources.add(callLogResource);
         return resources;
       }
     });
@@ -56,12 +61,10 @@ public class TelephonyWsMain {
     ServletHolder servletHolder = new ServletHolder(servletContainer);
     context.addServlet(servletHolder, "/*");
 
-    // Setup static file context (WEBAPP)
     WebAppContext webapp = new WebAppContext();
     webapp.setResourceBase("src/main/webapp");
     webapp.setContextPath("/");
 
-    // Setup http server
     ContextHandlerCollection contexts = new ContextHandlerCollection();
     contexts.setHandlers(new Handler[] { context, webapp });
     Server server = new Server(8080);
@@ -76,10 +79,8 @@ public class TelephonyWsMain {
   }
 
   private static ContactResource createContactResource() {
-    // Setup resources' dependencies (DOMAIN + INFRASTRUCTURE)
     ContactRepository contactRepository = new ContactRepositoryInMemory();
 
-    // For development ease
     if (isDev) {
       ContactDevDataFactory contactDevDataFactory = new ContactDevDataFactory();
       List<Contact> contacts = contactDevDataFactory.createMockData();
@@ -92,7 +93,18 @@ public class TelephonyWsMain {
     return new ContactResourceImpl(contactService);
   }
 
-  private static void createCallLogResource() {
-    // TODO something to do here!
+  private static CallLogResource createCallLogResource() {
+    CallLogRepository callLogRepository = new CallLogRepositoryInMemory();
+
+    if (isDev) {
+      CallLogDevDataFactory callLogDevDataFactory = new CallLogDevDataFactory();
+      List<CallLog> callLogs = callLogDevDataFactory.createMockData();
+      callLogs.stream().forEach(callLogRepository::save);
+    }
+
+    CallLogAssembler callLogAssembler = new CallLogAssembler();
+    CallLogService callLogService = new CallLogService(callLogRepository, callLogAssembler);
+
+    return new CallLogResourceImpl(callLogService);
   }
 }
--- src/main/java/ca/ulaval/glo4003/ws/api/calllog/CallLogResource.java
+++ src/main/java/ca/ulaval/glo4003/ws/api/calllog/CallLogResource.java
@@ -2,11 +2,18 @@ package ca.ulaval.glo4003.ws.api.calllog;
 
 import ca.ulaval.glo4003.ws.api.calllog.dto.CallLogDto;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.util.List;
 
+@Path("/telephony/calllogs")
 public interface CallLogResource {
 
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
   List<CallLogDto> getCallLogs();
 
-  void deleteCallLog(String id);
+  @DELETE
+  @Path("{id}")
+  void deleteCallLog(@PathParam("id") String id);
 }
